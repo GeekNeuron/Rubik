442252979649b11a68a45655bb119105750b1bed
Geek Neuron <geekneuron@gmail.com>
2026-09-14 12:41:51 +0330

    Add files via upload

--- libs/stackmat/event/timer-event.js
+++ libs/stackmat/event/timer-event.js
@@ -1,1 +1,17 @@
-
+const stringEventType = (arr) => arr;
+const types = stringEventType([
+    'packetReceived',
+    'timerConnected',
+    'timerDisconnected',
+    'started',
+    'stopped',
+    'reset',
+    'ready',
+    'unready',
+    'starting',
+    'leftHandDown',
+    'leftHandUp',
+    'rightHandDown',
+    'rightHandUp',
+]);
+export const isEvent = (x) => typeof x === 'string' && types.includes(x);
